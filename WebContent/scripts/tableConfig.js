191d56e7db315acec62b592ecbdf915d5164c4ab
efra1 <43354608+efra1@users.noreply.github.com>
2020-10-12 12:52:07 -0400

    Add files via upload

---
+++ WebContent/scripts/tableConfig.js
@@ -0,0 +1,26 @@
+var dataTableParams={
+		//"sDom":"<'row subido'<'col-xs-6'T><'col-xs-6'f>r>t<'row'<'col-xs-6'i><'col-xs-6'p>>",
+		"oLanguage": {
+    		"sLengthMenu":     "Mostrar _MENU_ registros",
+    		"sZeroRecords":    "No se encontraron resultados",
+    		"sEmptyTable":     "Ningun dato disponible en esta tabla",
+    		"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+    		"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+    		"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+    		"sInfoPostFix":    "",
+    		"sSearch":         "Buscar:",
+    		"sUrl":            "",
+    		"sInfoThousands":  ",",
+    		"sLoadingRecords": "Cargando...",
+    		"oPaginate": {
+        		"sFirst":    "Primero",
+	        	"sLast":     "Ultimo",
+    	    	"sNext":     "Siguiente",
+        		"sPrevious": "Anterior"
+		    },
+    		"oAria": {
+        		"sSortAscending":  ": Clic para ordenar la columna de manera ascendente",
+        		"sSortDescending": ": Clic para ordenar la columna de manera descendente"
+    		}	
+		}
+	}
